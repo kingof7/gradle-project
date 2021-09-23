5be5757b951d2bc3986d11ad4c322c0c3f7aa086
jhlee <jhlee@sorin.co.kr>
2021-09-23 14:57:40 +0900

    repository 구현

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # gradle-project
-gradle-project + Spring Boot
+gradle-project + Spring Boot + JPA
 
 fastcampus intellij 강의
--- src/main/java/org/example/TodoServerApplication.java
+++ src/main/java/org/example/TodoServerApplication.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.repository.TodoRepository;
+
 public class TodoServerApplication {
     public static void main(String[] args) {
-        System.out.println("Hello TODO");
+        System.out.println("HELLO TODO-SERVER");
+        TodoRepository repository;
+        //repository.save();
+
     }
 }
