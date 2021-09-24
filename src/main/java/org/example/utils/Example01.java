bb3d9aa6ba45bb33a5555bb4f1b1f170274d3292
jhlee <jhlee@sorin.co.kr>
2021-09-24 17:16:56 +0900

    debug mode

---
+++ src/main/java/org/example/utils/Example01.java
@@ -0,0 +1,9 @@
+package org.example.utils;
+
+public class Example01 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
